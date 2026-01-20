ef1e24cee17dcb5558ddb32d98fdae4f9b76d91a
jack-dark <decker3d@gmail.com>
2026-01-19 16:10:06 -0800

    feat: generate content recursively

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,6 @@
 import os
 
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
@@ -36,3 +37,21 @@ def extract_title(markdown: str):
             return line[2:].strip()
 
     raise ValueError("No H1 heading found. H1 heading is required.")
+
+
+def generate_pages_recursive(
+    dir_path_content: str,
+    template_path: str,
+    dest_dir_path: str,
+):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
